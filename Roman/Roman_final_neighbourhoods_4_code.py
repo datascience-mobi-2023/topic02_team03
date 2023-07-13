c8f167a5d6961cccb22ba1241302fb468ed439e6
Romiky7 <nl273@stud.uni-heidelberg.de>
2023-07-13 12:41:36 +0200

    work in progress

--- Roman/Roman_final_neighbourhoods_4_code.py
+++ Roman/Roman_final_neighbourhoods_4_code.py
@@ -151,17 +151,156 @@ for _, row in work_with_df_pos_bin_mutant_dms_score_mut_sequence.iterrows():
         Neighbourhood_extraction_result.append(neighbourhood_properties)
 
 Roman_2 = pd.DataFrame(Neighbourhood_extraction_result)
-#Roman_2
-# Drop duplicate rows with identical values
 Roman_2_filtered = Roman_2.drop_duplicates()
-# Print the filtered DataFrame
-#(Roman_2_filtered)
-Roman_2_filtered = pd.concat([Neighbourhood_results_df, Roman_2_filtered], axis=0, ignore_index=True)
-#print(Roman_2_filtered)
-Roman_3 = Roman_2_filtered.drop_duplicates()
-#Roman_3
-Roman_4 = Roman_3.drop(["pKx3"], axis=1)
-#Roman_4
-Roman_5 = Roman_4.drop_duplicates()
-print(Roman_5)
-#Endgültiges gereinigtes dataframe mit allen single mutanten mit allen nicht-duplikativen frame-Wanderungen. Zeile 0 bis 231 ist di nicht mutierte Referenz
+Roman_3 = pd.concat([Neighbourhood_results_df, Roman_2_filtered], ignore_index=True).drop_duplicates()
+Roman_5 = Roman_3.drop(["pKx3"], axis=1)
+
+# First preliminary version to work with.
+# It contains all single mutants with non-duplicated frame-shifts along the respective mutated sequence.
+# Rows 0-231 are the unmutated reference sequence as neighbourhoods.
+
+# Now it was quickly tested, whether the results make sense based on some easy calculations, and they do.
+# It was manually tested, whether the results make sense, in particular the amount of rows in the dataframe.
+# By calculating what the code does, the same amount of rows was calculated as are present.
+
+# Now the dataframe will undergo some severe changes until its finalized version:
+
+
+Roman_6 = pd.DataFrame(columns=["Neighbourhood"])
+columns_to_copy = ["Molecular Weight", "Residue Weight", "pKa1", "pKb2", "pl4", "H", "VSC", "P1", "P2", "SASA", "NCISC"]
+
+for index, row in Roman_5.iterrows():
+    neighbourhood = row["Neighbourhood"] #Neighbourhood-Wert jeder Zeile nehmen
+    neighbourhood_length = len(neighbourhood)
+
+    for i in range(len(unmutated_sequence) - 6): #entfernt wieder 6 Positionen damit es aufkommt und wandert über alle möglichen Positionen
+        mismatch_found = False #um zu tracken, ob es einen mismatch gibt
+
+        for j in range(neighbourhood_length): #läuft durch alle Positionen eines neighbourhoods
+            if j == 3: #aktuelle Position soll die vierte sein
+                if neighbourhood[j] == unmutated_sequence[i+j]: #Prüft ob die vierte AS die Position der original-Sequenz matched
+                    mismatch_found = True #wenn match, dann True
+                    break # Bricht den loop, wenn die vierte Position matched
+            else:
+                if neighbourhood[j] != unmutated_sequence[i+j]: #prüft ob die die NICHT vierten AS die Position der original-Sequenz NICHT matched
+                   mismatch_found = True #Wenn eine AS außerhalb der vierten Position nicht matched, dann True
+                   break # Bricht den loop, wenn eine andere Position außer der vierten nicht matched
+
+        if not mismatch_found: #wenn kein mismatch außer in der vierten Position
+            Roman_6 = pd.concat([Roman_6, pd.DataFrame({"Neighbourhood": [neighbourhood]})], ignore_index=True) #wenn kein mismatch, dann Neighbourhood dem neuen Dataframe hinzufügen
+            break #Weil ein match gefunden wurde, abbrechen, weil es keinen zweiten Match geben kann
+
+Roman_6 = Roman_6.join(Roman_5[columns_to_copy]) #gibt mir meine alten Spalten dazu
+
+
+Roman_7 = pd.DataFrame(columns=["Neighbourhood"] + columns_to_copy)
+
+rows = []
+for _, row in Roman_6.iterrows():
+    neighbourhood = row["Neighbourhood"]
+    unmutated_row = neighbourhood[:3] + unmutated_sequence[3] + neighbourhood[4:]
+    properties = {col: row[col] for col in columns_to_copy}
+    mutated_df = pd.DataFrame(data=[[neighbourhood] + list(row[columns_to_copy])], columns=["Neighbourhood"] + columns_to_copy)
+    unmutated_df = pd.DataFrame(data=[[unmutated_row] + list(properties.values())], columns=["Neighbourhood"] + columns_to_copy)
+    rows.append(mutated_df)
+    rows.append(unmutated_df)
+
+Roman_7 = pd.concat(rows, ignore_index=True)
+
+#klappt mit fehler aber concat
+#Fehler, der nimmt immer die 3. Position der unmutierten Sequenz absolut, aber nicht relativ in dem Bereich, wo die neighbourhood ist
+
+property_names = ["Molecular Weight", "Residue Weight", "pKa1", "pKb2", "pl4", "H"]  # List of property names to calculate
+
+# Calculate the property values for each row in Roman_7 and update the corresponding columns
+Roman_8 = Roman_7.copy()  # Create a copy of Roman_7
+for property_name in property_names:
+    Roman_8[property_name] = Roman_8["Neighbourhood"].apply(lambda x: sum(ASE_clear[property_name].loc[ASE_clear["Letter"] == aa].values[0] for aa in x))
+    #lambda definiert schnell eine Funktion ohne es vorher mit "def" machen zu müssen. lambda argument: expression
+       #This function calculates the sum of specific property values for each letter in the "Neighbourhood" value.
+    #loc sucht den richtigen Buchstaben und nimmt davon die Werte der Spalten
+    #values 0 nimmt den ersten Wert
+
+#Berechnet mir alle Werte für die neu eingetragenen unmutierten Sequenzen und trägt sie ein.
+#Roman_8 ist die Version, woran man alles sehen kann.
+#Alle Mutationen vor AS 4 und nach AS 235 können nicht angezeigt werden, weil sie nie in der Mitte sein können.
+
+sequence = "MSKGEEL"
+molecular_weight = sum(ASE_clear["Molecular Weight"].loc[ASE_clear["Letter"] == aa].values[0] for aa in sequence)
+print("Molecular Weight:", molecular_weight)
+#Zum Überprüfen der Berechnung
+
+# Create a new DataFrame Roman_9 as a copy of Roman_8
+Roman_9 = Roman_8.copy()
+
+# Filter out mutations where 'n' in 'XnY' mutation name is 3
+filtered_mutations = [mutant for mutant in single_mutations_df['mutant'] if not (len(mutant) == 3 and mutant[1] == '3')]
+
+# Clone each mutant and write twice consecutively in a new list
+mutants_cloned = [mutant for mutant in filtered_mutations for _ in range(2)]
+
+# Truncate the list of mutants to match the length of the DataFrame
+mutants_cloned = mutants_cloned[:len(Roman_9)]
+
+# Insert the 'Mutation' column left of the 'Neighbourhood' column
+Roman_9.insert(Roman_9.columns.get_loc('Neighbourhood'), 'Mutation', mutants_cloned)
+
+# Create a copy of the original DataFrame
+Roman_10 = Roman_9.copy()
+
+# Create a list to store the modified mutation names
+modified_mutations_name = []
+modified_neighbourhood = []
+
+for index, row in Roman_9.iterrows():
+    mutation = row['Mutation']
+    neighbourhood = row['Neighbourhood']
+
+    # Extract the X character from the mutation name
+    X = mutation[0]
+
+    # Modify the 4th position of the neighbourhood sequence based on X for odd rows
+    if index % 2 != 0:
+        modified_neighbourhood.append(neighbourhood[:3] + X + neighbourhood[4:])
+    else:
+        modified_neighbourhood.append(neighbourhood)
+
+    # Append '-unmut' to every other mutation name
+    if index % 2 != 0:
+        modified_mutations_name.append(mutation + '-unmut')
+    else:
+        modified_mutations_name.append(mutation)
+
+# Update the 'Mutation' and 'Neighbourhood' columns with the modified values
+Roman_10['Mutation'] = modified_mutations_name
+Roman_10['Neighbourhood'] = modified_neighbourhood
+
+
+
+property_names = ["Molecular Weight", "Residue Weight", "pKa1", "pKb2", "pl4", "H", "VSC", "P1", "P2", "SASA", "NCISC"]  # List of property names to calculate
+
+# Calculate the property values for each row in Roman_7 and update the corresponding columns
+Roman_11 = Roman_10.copy()  # Create a copy of Roman_7
+for property_name in property_names:
+    Roman_11[property_name] = Roman_11["Neighbourhood"].apply(lambda x: sum(ASE_clear[property_name].loc[ASE_clear["Letter"] == aa].values[0] for aa in x))
+    #lambda definiert schnell eine Funktion ohne es vorher mit "def" machen zu müssen. lambda argument: expression
+       #This function calculates the sum of specific property values for each letter in the "Neighbourhood" value.
+    #loc sucht den richtigen Buchstaben und nimmt davon die Werte der Spalten
+    #values 0 nimmt den ersten Wert
+
+Roman_11
+#Berechnet mir alle Werte für die neu eingetragenen unmutierten Sequenzen und trägt sie ein. Macht es nochmal, um sicherzugehen
+#RECHENWERTE STIMMEN!!!!!
+#Roman_11 ist die Version, woran man alles sehen kann.
+#Alle Mutationen vor AS 4 und nach AS 235 können nicht angezeigt werden, weil sie nie in der Mitte sein können.
+
+#Roman_11 nach gewünschter Mutation filtern
+
+
+mutation_name = input("Enter the mutation name: ")
+#öffnet ein kleines Fenster zum Eintippen
+
+what_I_see_df = Roman_11[Roman_11['Mutation'].str.contains(mutation_name)]
+print(what_I_see_df)
+#Funktioniert, muss nur den Code vorher fixen um richtige Werte zu erzeugen
+
