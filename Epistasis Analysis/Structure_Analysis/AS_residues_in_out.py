b718f79b166a0729b70590c08b14343b28318c2e
tianxinangelama <131357979+tianxinangelama@users.noreply.github.com>
2023-07-16 15:38:16 +0200

    update

--- Epistasis Analysis/Structure_Analysis/AS_residues_in_out.py
+++ Epistasis Analysis/Structure_Analysis/AS_residues_in_out.py
@@ -2,4 +2,9 @@
 def split_m1_to_m15(df):
     df[['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'm11', 'm12', 'm13', 'm14', 'm15']] = \
         df['mutant'].str.split(':', 14, expand=True)
+    return df
+
+def reduce_df_to_DMSscore_mutations(df):
+    df = df.drop(['mutant', 'mutated_sequence', 'DMS_score_bin'], axis=1)
+    return df
 
