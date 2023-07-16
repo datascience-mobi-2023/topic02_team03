2dc954c26343eb559e6bc19f520fb450b144ce93
tianxinangelama <131357979+tianxinangelama@users.noreply.github.com>
2023-07-16 15:29:56 +0200

    update

---
+++ Epistasis Analysis/Structure_Analysis/AS_residues_in_out.py
@@ -0,0 +1,6 @@
+# split into multiple columns, 1 column for every mutation
+def split_m1_to_m15(df):
+    df[['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'm11', 'm12', 'm13', 'm14', 'm15']] = \
+        df['mutant'].str.split(':', 14, expand=True)
+    return df
+
